25e0ac817bb716aa547c3adfe03a644d27291e92
Hani Sehweil <hsehweil@amazon.com>
2019-10-08 22:37:15 -0700

    Revert "Print merged Schema with directives. (#5)"
    This reverts commit 7150eae61d81637eb9f71ddca06494927e8c1292.
    [SUMMARY]
    Revertiing this because of the issues:
    1- seeing "undefined" in the merged schema.
    2- directives are not being merged under the extended types.

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as globUtil from 'glob';
-import { GraphQLSchema, isSpecifiedDirective, isSpecifiedScalarType, print } from 'graphql';
 
 export function readGlob(pattern: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
@@ -19,29 +18,4 @@ export function readFile(file: string): Promise<string> {
       err ? reject(err) : resolve(data),
     );
   });
-}
-
-/**
- * This method is a workaround for the issue of not being able to print the
- * directives fields for the merged schema when using printSchema method
- * @param schema
- */
-export function printSchemaWithDirectives(schema: GraphQLSchema): string {
-  const str = Object
-    .keys(schema.getTypeMap())
-    .filter((k) => !k.match(/^__/))
-    .reduce((accum, name) => {
-      const type = schema.getType(name);
-      return !isSpecifiedScalarType(type)
-        ? accum += `${print(type.astNode)}\n`
-        : accum;
-    }, '');
-
-  return schema
-    .getDirectives()
-    .reduce((accum, d) => {
-      return !isSpecifiedDirective(d)
-        ? accum += `${print(d.astNode)}\n`
-        : accum;
-    }, str + `${print(schema.astNode)}\n`);
 }
